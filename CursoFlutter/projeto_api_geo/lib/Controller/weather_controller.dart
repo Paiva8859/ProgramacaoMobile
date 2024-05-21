9e33cacbef3864ad577d812ed070de27967ed700
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-05-21 16:03:21 -0300

    a

---
+++ CursoFlutter/projeto_api_geo/lib/Controller/weather_controller.dart
@@ -0,0 +1,33 @@
+import 'package:projeto_api_geo/Service/weather_service.dart';
+
+import '../Model/weather_model.dart';
+
+class WeatherController {
+  final List<Weather> _weatherList = [];
+  //get
+  List<Weather> get weatherList => _weatherList;
+
+  final WeatherService _service = WeatherService();
+
+  //métodos
+  //cidade
+  Future<void> getWeather(String city) async{
+    try {
+       Weather weather = Weather.fromJson(await _service.getWeather(city));
+       _weatherList.add(weather);
+    } catch (e) {
+      print(e);
+    }
+  }
+  //geolocation
+  Future<void> getWeatherbyLocation(double lat, double lon) async{
+    try {
+       Weather weather = Weather.fromJson(
+        await _service.getWeatherbyLocation(lat, lon)
+        );
+       _weatherList.add(weather);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
